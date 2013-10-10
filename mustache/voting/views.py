20267f67365797cd7d0b36f0d8605cee4044a69b
chryso <chryso@gmail.com>
2013-10-10 16:09:37 +0000

    added some testing views to get things going

--- mustache/voting/views.py
+++ mustache/voting/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(req):
+    return render(req, 'voting/home.html', {})
+
+def test(req):
+    return render(req, 'voting/test.html', {})
