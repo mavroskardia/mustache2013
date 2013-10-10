1b2683ee7a8ed305f2c70acb9b1231ba6eb910f4
Andy <chryso@gmail.com>
2013-10-10 17:17:38 -0400

    Added initial models

--- mustache/voting/models.py
+++ mustache/voting/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Gentleman(models.Model):
+	# Make Gentleman a profile model based on Django's User model
+	user = models.OneToOneField(User)
+	
+	name = models.CharField(max_length=256)
+	tagline = models.CharField(max_length=1024)
+	before_pic = models.ImageField(upload_to='images')
+	after_pic = models.ImageField(upload_to='images')
+	
+	votes = models.ForeignKey('Vote')
+
+class Vote(models.Model):
+	execution = models.IntegerField()
+	grooming = models.IntegerField()
+	creativity = models.IntegerField()
