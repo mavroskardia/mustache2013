47c27c952808cfccacf06fc3fa0fb735d868ca5d
Andy <chryso@gmail.com>
2013-10-10 17:59:00 -0400

    Added some startup settings and scaffolding for thumbnai resizing

--- mustache/mustache/settings.py
+++ mustache/mustache/settings.py
@@ -1,5 +1,9 @@
 # Django settings for mustache project.
 
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -50,18 +54,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
--- mustache/voting/models.py
+++ mustache/voting/models.py
@@ -6,13 +6,37 @@ class Gentleman(models.Model):
 	user = models.OneToOneField(User)
 	
 	name = models.CharField(max_length=256)
-	tagline = models.CharField(max_length=1024)
+	tagline = models.CharField(max_length=1024,blank=True)
 	before_pic = models.ImageField(upload_to='images')
-	after_pic = models.ImageField(upload_to='images')
-	
-	votes = models.ForeignKey('Vote')
+	before_pic_resized = models.ImageField(upload_to='images',blank=True)	
+	after_pic = models.ImageField(upload_to='images',blank=True)
+	after_pic_resized = models.ImageField(upload_to='images',blank=True)
+
+	def save(self, *args, **kwargs):
+#		self.resize_if_needed()
+		super(Gentleman, self).save(*args, **kwargs)
+
+	def __str__(self):
+		return self.__unicode__()
+
+	def __unicode__(self):
+		return self.name
+
+	def resize_if_needed(self):
+		if not self.before_pic_resized:
+			self.before_pic_resized = utils.create_thumbnail(self.before_pic)
+
+		if self.after_pic and not self.after_pic_resized:
+			self.after_pic_resized = utils.create_thumbnail(self.after_pic)
 
 class Vote(models.Model):
-	execution = models.IntegerField()
-	grooming = models.IntegerField()
-	creativity = models.IntegerField()
+	gentleman = models.OneToOneField(Gentleman)
+	execution = models.IntegerField(default=0)
+	grooming = models.IntegerField(default=0)
+	creativity = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.__unicode__()
+
+	def __unicode__(self):
+		return '%s: Execution: %s Grooming: %s Creativity: %s' % (self.gentleman.name, self.execution, self.grooming, self.creativity)
