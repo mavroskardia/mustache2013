018b312ae5b642e885042e9fea406b7b7fe6f897
Andy <chryso@gmail.com>
2013-10-12 12:14:45 -0400

    Putting it all together

--- mustache/voting/views.py
+++ mustache/voting/views.py
@@ -3,8 +3,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from models import Gentleman
+
+
 def home(req):
-    return render(req, 'voting/home.html', {'players':[1,2,3]})
+    return render(req, 'voting/home.html', {'gentlemen': Gentleman.objects.all })
 
 def test(req):
     return render(req, 'voting/test.html', {})
