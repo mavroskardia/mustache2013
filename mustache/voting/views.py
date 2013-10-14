7af155d89b5d550a61292bea9c818f3c0115b45a
chryso <chryso@gmail.com>
2013-10-14 03:59:22 +0000

    messing with mockup

--- mustache/voting/views.py
+++ mustache/voting/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(req):
-    return render(req, 'voting/home.html', {})
+    return render(req, 'voting/home.html', {'players':[1,2,3]})
 
 def test(req):
     return render(req, 'voting/test.html', {})
